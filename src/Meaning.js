2cdddd6a46681f4d737546ff23826b4a3a1d69e5
SofiaEn9 <sofiaanen9@gmail.com>
2023-09-22 11:30:48 -0600

    Add Example to keyword definition

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,12 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
       {/* SHOW ONLY ONE DEFINITION OPTION */}
-      <p>{props.meaning.definitions[0].definition}</p>
+      <p>
+        {props.meaning.definitions[0].definition}
+        <br />
+        <strong>Example:</strong>
+        <em>{props.meaning.definitions[0].example}</em>
+      </p>
       <Synonyms synonyms={props.meaning.definitions[0].definition.synonyms} />
     </div>
   );
